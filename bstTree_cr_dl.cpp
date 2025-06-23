f260ff9f40948e3bd49102b14579e9b86cd820bf
SWASTIK <165389491+SWASTIKpy@users.noreply.github.com>
2025-06-23 09:04:41 +0000

    deletion code added

--- bstTree_cr_dl.cpp
+++ bstTree_cr_dl.cpp
@@ -14,4 +14,38 @@ node * insert(node * root, int value)
 
 
 // deletion 
-
+node * deleteNode(node * root, int value)
+{
+    if(root == NULL)
+     return root;
+    
+     if(root -> data < value)
+        root -> right = deleteNode(root -> right, value)
+    else if(root -> data > value)
+        root -> left = deleteNode(root -> left, value);
+    else
+    {
+        // node with one child or no child
+        if(root -> left == NULL)
+        {
+            node * temp = root -> right;
+            delete root;
+            return temp;
+        }
+        else if(root -> right == NULL)
+        {
+            node * temp = root -> left;
+            delete root;
+            return temp;
+        }
+        
+        // node with two children: get the inorder successor (smallest in the right subtree)
+        node * temp = minValueNode(root -> right);
+        
+        // copy the inorder successor's content to this node
+        root -> data = temp -> data;
+        
+        // delete the inorder successor
+        root -> right = deleteNode(root -> right, temp -> data);
+    }
+}
